1aef10ac9c6acbb5e69953e1c9d44ecc784cb5a4
dbianchii <dbianchii@gmail.com>
2024-08-08 01:17:46 -0300

    zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { create } from "zustand";
 
-function App() {
-  const [count, setCount] = useState(0)
+type State = {
+  firstName: string;
+  lastName: string;
+};
+
+type Action = {
+  updateFirstName: (firstName: State["firstName"]) => void;
+  updateLastName: (lastName: State["lastName"]) => void;
+};
+
+// Create your store, which includes both state and (optionally) actions
+const usePersonStore = create<State & Action>((set) => ({
+  firstName: "",
+  lastName: "",
+  updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+  updateLastName: (lastName) => set(() => ({ lastName: lastName })),
+}));
+
+// In consuming app
+export default function App() {
+  // "select" the needed state and actions, in this case, the firstName value
+  // and the action updateFirstName
+  const firstName = usePersonStore((state) => state.firstName);
+  const updateFirstName = usePersonStore((state) => state.updateFirstName);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <label>
+        First name
+        <input
+          // Update the "firstName" state
+          onChange={(e) => updateFirstName(e.currentTarget.value)}
+          value={firstName}
+        />
+      </label>
 
-export default App
+      <p>
+        Hello, <strong>{firstName}!</strong>
+      </p>
+    </main>
+  );
+}
